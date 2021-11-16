b911ff659910c97547763c15f26a692a6fee3109
A Gordeev <nonameforman1@yandex.ru>
2021-11-16 20:43:05 +0300

    version1

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,6 @@ const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__button-close")
 const popup = document.querySelector(".popup");
 
-let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input-box_value_name");
-let aboutInput = document.querySelector(".popup__input-box_value_about");
-const saveButton = document.querySelector(".popup__button-save");
-let nameValue = document.querySelector(".profile__name");
-let aboutValue = document.querySelector(".profile__about");
-
-
-console.log(nameInput.value);
-console.log(nameValue.textContent);
-
-
 function open() {
     popup.classList.add("popup_opened");
 }
@@ -22,12 +10,19 @@ function close() {
     popup.classList.remove("popup_opened");
 }
 
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input-box_value_name");
+let aboutInput = document.querySelector(".popup__input-box_value_about");
+let nameValue = document.querySelector(".profile__name");
+let aboutValue = document.querySelector(".profile__about");
+
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    nameInput.value = 
+    nameValue.textContent = nameInput.value;
+    aboutValue.textContent = aboutInput.value;
+    close()
 }
-
-formSubmitHandler (evt);
 
 editButton.addEventListener("click", open);
 closeButton.addEventListener("click", close);
+formElement.addEventListener("submit", formSubmitHandler);
