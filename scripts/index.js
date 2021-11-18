8edaaa3f92a6174ccefad411ef37ea9177559965
A Gordeev <nonameforman1@yandex.ru>
2021-11-18 17:37:33 +0300

    mesto version2

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,22 @@ const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__button-close")
 const popup = document.querySelector(".popup");
 
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input-box_value_name");
+let aboutInput = document.querySelector(".popup__input-box_value_about");
+let nameValue = document.querySelector(".profile__name");
+let aboutValue = document.querySelector(".profile__about");
+
 function open() {
     popup.classList.add("popup_opened");
+    nameInput.value = nameValue.textContent.trim();
+    aboutInput.value = aboutValue.textContent.trim();
 }
 
 function close() {
     popup.classList.remove("popup_opened");
 }
 
-let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input-box_value_name");
-let aboutInput = document.querySelector(".popup__input-box_value_about");
-let nameValue = document.querySelector(".profile__name");
-let aboutValue = document.querySelector(".profile__about");
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
