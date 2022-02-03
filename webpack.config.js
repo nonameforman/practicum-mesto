2de295d6c4fbeee9cb62573ff776d4ccfb7a3edb
A Gordeev <nonameforman1@yandex.ru>
2022-02-03 20:58:31 +0300

    mesto 3.02 webpack edition

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: {main: "./src/index.js"},
-    outpath: {
+    entry: {main: "./src/scripts/index.js"},
+    output: {
         path: path.resolve(__dirname, "dist"),
         filename: "main.js",
         publicPath: ''
     },
     mode: "development",
     devServer: {
-        contentBase: path.resolve(__dirname, './dist'),
+        static: path.resolve(__dirname, './dist'),
         compress: true,
         port: 8080,
         open: true
@@ -20,7 +23,26 @@ module.exports = {
             test: /\.js$/,
             use: 'babel-loader',
             exclude: '/node_modules/'
+          },
+          {
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource'
+          },
+          {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, {
+              loader: 'css-loader',
+              options: { importLoaders: 1 }
+            },
+            'postcss-loader']
           }
-          ]
-      }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ]
 }
